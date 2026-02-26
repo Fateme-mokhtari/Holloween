dac7a519a554f071ddfb06688786ed2bb16fee77
Fatemeh <fatememokhtari.7020@gmail.com>
2026-02-26 12:02:07 +0100

    update API endpoint- Added Next.js -rewrite for API proxying in next.config.mjs - Updated getAllHouses API endpoint to remove /admin prefix

--- lib/apiHouses.ts
+++ lib/apiHouses.ts
@@ -2,5 +2,5 @@ import { apiClient } from './apiClient';
 import { Houses } from '@/.next/types/houses';
 
 export async function getAllHouses(): Promise<Houses[]> {
-  return apiClient.get('/admin/index.php?endpoint=getHouses');
+  return apiClient.get('/index.php?endpoint=getHouses');
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // When you fetch from "/api-proxy/...", Next.js handles it on the server
+        source: '/api-proxy/:path*',
+        destination: 'https://halloween.shafi.ee/:path*', 
+      },
+    ]
+  },
 };
 
 export default nextConfig;
