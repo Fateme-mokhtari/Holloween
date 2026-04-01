73b4202a344df3cee523f4cad466528abd425c43
Fatemeh <fatememokhtari.7020@gmail.com>
2026-04-01 10:58:41 +0200

    fix(apiClient): remove fallback for API base URL

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,5 +1,4 @@
-const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 type ApiRequestOptions = RequestInit;
 
